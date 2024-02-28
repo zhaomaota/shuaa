7d48e1e8c9127272f9955a495e09af52dfb17919
zhaomaota <409024813@qq.com>
2024-02-28 18:17:59 +0800

    first commit

---
+++ src/pages/list/card-list/service.ts
@@ -0,0 +1,13 @@
+import { request } from '@umijs/max';
+import type { CardListItemDataType } from './data.d';
+
+export async function queryFakeList(params: {
+  count: number;
+}): Promise<{ data: { list: CardListItemDataType[] } }> {
+  return request('/api/card_fake_list', {
+    params,
+  });
+}
+export async function getAllData() {
+  return request('/bpi/student/getAllData')
+}
